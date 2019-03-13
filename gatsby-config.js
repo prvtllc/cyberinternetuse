ac597b799ea895c1d01847e07e6979297148a324
Domingo Rivera <djr@infosecusa.com>
2019-03-12 20:32:10 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,6 +67,22 @@ module.exports = {
           },
 
           {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+              name: `GatsbyJS`,
+              short_name: `GatsbyJS`,
+              start_url: `/`,
+              background_color: `#f7f0eb`,
+              theme_color: `#a2466c`,
+              // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+              // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+              display: `standalone`,
+              icon: `src/images/icon.png`, // This path is relative to the root of the site.
+              include_favicon: true, // Include favicon
+            },
+          },
+
+          {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 590,
